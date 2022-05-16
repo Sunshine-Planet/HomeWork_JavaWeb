84e5eaa578742f42dd24fbc113f8d0fe6c4fc6d5
Sunshine-Planet <sunshine.p.2002@gmail.com>
2022-05-16 17:23:15 +0800

    Revert "Complete C18 DateBase DAO exam."
    This reverts commit 53afe5412f9e138d2d76cf1aafe3937b30489de8.

--- TomcatServer/src/c18/dao/IWorkerDAO.java
+++ TomcatServer/src/c18/dao/IWorkerDAO.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IWorkerDAO {
-    public boolean doCreate(Worker work) throws Exception;
-    public List<Worker> findAll(String keyWord) throws Exception;
-    public Worker findById(int empno) throws Exception;
+    public boolean doCreate(Worker worker) throws SQLException;
+    public List<Worker> findAll(String keyWord);
+    public Worker findById(int empno);
 }
